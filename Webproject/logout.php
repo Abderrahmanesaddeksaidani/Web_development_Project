2b7ac75e558c45dc59acf32407c81da2cc256228
Abderrahmane Saddek Saidani <abderrahmanesaddek.saidani@univ-bba.dz>
2026-04-30 18:42:16 +0100

    Delete Webproject/logout.php

--- Webproject/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start(); // Start the session to find it
-session_unset(); // Remove all session variables
-session_destroy(); // Destroy the session itself
-
-// Redirect back to the login page
-header("Location: login.php");
-exit;
-?>
